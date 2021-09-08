a4b430b2075fa31fd05660f821fcb7f64a8f42ef
karri-pooja <karripooja540@gmail.com>
2021-09-08 11:53:06 -0500

    first commit

---
+++ src/main/java/com/demo/git_jenkins_docker_demo/App.java
@@ -0,0 +1,13 @@
+package com.demo.git_jenkins_docker_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
